7f4308beee8f85229959f6630ee0c540664ff8dd
Jeffrey D <11084623+Nephery@users.noreply.github.com>
2026-03-24 23:45:54 -0400

    fix pubsub+ wait strategy

--- src/integrationTest/java/com/solace/connector/kafka/connect/source/it/util/extensions/pubsubplus/pubsubplus/NetworkPubSubPlusContainerProvider.java
+++ src/integrationTest/java/com/solace/connector/kafka/connect/source/it/util/extensions/pubsubplus/pubsubplus/NetworkPubSubPlusContainerProvider.java
@@ -4,7 +4,9 @@ import com.solace.test.integration.junit.jupiter.extension.pubsubplus.provider.c
 import com.solace.test.integration.testcontainer.PubSubPlusContainer;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.testcontainers.containers.Network;
+import org.testcontainers.containers.wait.strategy.Wait;
 
+import java.time.Duration;
 import java.util.function.Supplier;
 
 public class NetworkPubSubPlusContainerProvider extends SimpleContainerProvider {
@@ -15,6 +17,13 @@ public class NetworkPubSubPlusContainerProvider extends SimpleContainerProvider 
 	public Supplier<PubSubPlusContainer> containerSupplier(ExtensionContext extensionContext) {
 		return () -> new PubSubPlusContainer()
 				.withNetwork(DOCKER_NET)
-				.withNetworkAliases(DOCKER_NET_PUBSUB_ALIAS);
+				.withNetworkAliases(DOCKER_NET_PUBSUB_ALIAS)
+
+				// Workaround for inadequate wait strategy until we update to a fixed version of test support
+				.withExposedPorts(5550)  // Expose health check port
+				.waitingFor(Wait.forHttp("/health-check/guaranteed-active")
+						.forPort(5550)
+						.forStatusCode(200)
+						.withStartupTimeout(Duration.ofMinutes(5)));
 	}
 }
